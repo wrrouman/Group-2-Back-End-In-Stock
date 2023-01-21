d58cce387d7c7cfc743a2c0121ef17f01fd95470
wrrouman <106677200+wrrouman@users.noreply.github.com>
2023-01-21 12:59:11 -0500

    Will-Backend-components

--- routes/InStock.js
+++ routes/InStock.js
@@ -85,52 +85,113 @@ router.delete("/warehouses/:warehouseId", (req, res) => {
 
 
 //Function to read warehouse.json
-function readWarehouse() {
-    const warehouseFile = fs.readFileSync(WarehouseDetails);
-    const warehouseData = JSON.parse(warehouseFile);
-    return warehouseData;
-}
+// function readWarehouse() {
+//     const warehouseFile = fs.readFileSync(WarehouseDetails);
+//     const warehouseData = JSON.parse(warehouseFile);
+//     return warehouseData;
+// }
 
-//Function to write to warehouse.json
-function writeWarehouse(data) {
-    const stringifiedData = JSON.stringify(data);
-    fs.writeFileSync(WarehouseDetails, stringifiedData);
-}
+// //Function to write to warehouse.json
+// function writeWarehouse(data) {
+//     const stringifiedData = JSON.stringify(data);
+//     fs.writeFileSync(WarehouseDetails, stringifiedData);
+// }
 
-//defining /warehouses route and returning the .get request for the route as json
-router.get("/warehouses/:warehouseid", (_req, res) => {
-    const warehouseData = readVideos();
-    const strippedData = warehouseData((warehouse) => {
-        return {
-            id: warehouse.id,
-            name: warehouse.name,
-            address: warehouse.address,
-            city: warehouse.city,
-            country: warehouse.country,
-            contact: warehouse.contact,
-            name: warehouse.name,
-            position warehouse.position,
-            phone: warehouse.phone,
-            email: warehouse.email
-        };
-    });
-    res.json(strippedData);
+// //defining /warehouses route and returning the .get request for the route as json
+// router.get("/warehouses/:warehouseid", (_req, res) => {
+//     const warehouseData = readVideos();
+//     const strippedData = warehouseData((warehouse) => {
+//         return {
+//             id: warehouse.id,
+//             name: warehouse.name,
+//             address: warehouse.address,
+//             city: warehouse.city,
+//             country: warehouse.country,
+//             contact: warehouse.contact,
+//             name: warehouse.name,
+//             position warehouse.position,
+//             phone: warehouse.phone,
+//             email: warehouse.email
+//         };
+//     });
+//     res.json(strippedData);
+// });
+
+// //defining /warehouses route and adding the .post request for the route in videos json
+// router.post("/videos", (req, res) => {
+//     const videosData = readVideos();
+//     const newVideo = {
+//         id: uniqid(),
+//         title: req.body.title,
+//         channel: "placeholder",
+//         image: req.body.image,
+//         description: req.body.description,
+//         comments: []
+//     };
+//     videosData.push(newVideo);
+//     writeVideos(videosData);
+//     res.status(201).json(newVideo);
+// });
+
+
+
+// Will's Backend components 
+
+// Get List of All Inventories
+router.get("/inventories", (req, res) => {
+  let allinventory_data = fs.readFileSync("data/inventories.json");
+  let parse_allinventory_data = JSON.parse(allinventory_data);
+  res.json(parse_allinventory_data);
 });
 
-//defining /warehouses route and adding the .post request for the route in videos json
-router.post("/videos", (req, res) => {
-    const videosData = readVideos();
-    const newVideo = {
-        id: uniqid(),
-        title: req.body.title,
-        channel: "placeholder",
-        image: req.body.image,
-        description: req.body.description,
-        comments: []
-    };
-    videosData.push(newVideo);
-    writeVideos(videosData);
-    res.status(201).json(newVideo);
+
+
+
+// Get Inventory Items for a Warehouse
+router.get("/inventories/:warehouseId/:inventoriesId", (req, res) => {
+  const warehouseId = req.params.warehouseId;
+  const inventoriesId = req.params.inventoriesId;
+  let allinventory_data = fs.readFileSync("data/inventories.json");
+  let parse_allinventory_data = JSON.parse(allinventory_data);
+  
+  //find inventory items that belong to the same warehouse
+  let currentWarehouseDetails = parse_allinventory_data.filter(
+    (inventory) => inventory.warehouseID === warehouseId
+  );
+
+  let currentInventoryincurrentWarehouseDetails = currentWarehouseDetails.find(
+    (inventory) => inventory.id === inventoriesId
+  );
+
+  res.json(currentInventoryincurrentWarehouseDetails);
+
+});
+
+
+
+
+// DELETE an Inventory Item
+router.delete("/inventories/:inventoriesId", (req, res) => {
+  const inventoriesId = req.params.inventoriesId;
+  let allinventory_data = fs.readFileSync("data/inventories.json");
+  let parse_allinventory_data = JSON.parse(allinventory_data);
+
+  //find details of Item we want to delete
+  let currentInventoryDetails = parse_allinventory_data.find(
+    (inventory) => inventory.id === inventoriesId
+  );
+  //find index of item we want to delete
+  let currentInventoryDetails_Index = parse_allinventory_data.findIndex(
+    (inventory) => inventory.id === inventory.Id
+  );
+
+  //delete Item
+  parse_allinventory_data.splice(currentInventoryDetails_Index, 1);
+  let stringify_allinventory_data = JSON.stringify(parse_allinventory_data);
+  fs.writeFileSync("data/inventories.json", stringify_allinventory_data);
+  res
+    .status(201)
+    .send(`Deleted ${currentInventoryDetails.itemName} Successfully!`);
 });
 
 //exports the route to be used (similar to a component in react) on the server js index.js
